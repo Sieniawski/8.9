f3d7f542e2450716c7706a4890cf3fb7b414f365
Wojciech <sieniawski7@wp.pl>
2017-02-06 15:44:01 +0100

    Update 8.9script.js
    poprawione;
     niepotrzebny enter w 2. linijce
     brak średnika po wywołaniu funkcji console.log oraz makeTree
     zakomentowany kod

--- js/8.9script.js
+++ js/8.9script.js
@@ -1,14 +1,10 @@
-function makeTree (height) {
-	
+function makeTree (height) {	
 	for (var i = 0; i <= height; i++) {
 		var star = '';
 		for (var j = 1; j < i; j++) {
 			star += '*';
 		}
-		console.log(star)
+		console.log(star);
 	}
 }
-
-makeTree(10)
-
-// for ( wartość_inicjująca ; warunek_stopu ; modyfikacja_wartości)
+makeTree(10);
